064657dac62f0ecf74dc50e288f4e44deb38ed76
oyerohabib <oyerohabib@yahoo.com>
2025-03-02 10:59:07 +0100

    feat: defined all types for various components

---
+++ src/app/types/index.ts
@@ -0,0 +1,43 @@
+export interface ButtonProps {
+  text: string;
+  primary?: boolean;
+  onClick?: () => void;
+  className?: string;
+}
+
+export interface NavItemProps {
+  title: string;
+  href: string;
+  active?: boolean;
+}
+
+export interface AlertBannerProps {
+  text: string;
+  linkText: string;
+  href: string;
+}
+
+export interface FeatureListItemProps {
+  text: string;
+}
+
+export interface FeatureListProps {
+  items: string[];
+}
+
+export interface RegisterFormProps {
+  onSubmit: (data: RegisterFormData) => void;
+}
+
+export interface RegisterFormData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  telegramUser: string;
+}
+
+export interface BenefitCardProps {
+  icon: React.ReactNode;
+  title: string;
+  items: string[];
+}
