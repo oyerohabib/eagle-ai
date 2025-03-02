e0b4733d75f0d1ffc073d9e9c7b08327c3bbf71d
oyerohabib <oyerohabib@yahoo.com>
2025-03-02 22:07:07 +0100

    feat: added open sans font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Poppins } from "next/font/google";
+import { Poppins, Open_Sans } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const poppins = Poppins({
   variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: "400",
+});
+
+const openSans = Open_Sans({
+  variable: "--font-openSans",
   subsets: ["latin"],
   weight: "400",
 });
@@ -30,9 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable}`}
-      >
+      <body className={`${poppins.variable} ${openSans.variable}`}>
         {children}
       </body>
     </html>
