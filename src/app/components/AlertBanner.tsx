c97d25d7d70a4a08d7a00a89a05a224efee1c0a8
oyerohabib <oyerohabib@yahoo.com>
2025-03-02 14:06:03 +0100

    style: improved the alert banner to match the figma design

--- src/app/components/AlertBanner.tsx
+++ src/app/components/AlertBanner.tsx
@@ -2,16 +2,14 @@ import Link from "next/link";
 
 const AlertBanner: React.FC = () => {
   return (
-    <div className="w-full bg-banner text-navy-900 py-2 px-4 text-center">
-      <div className="container mx-auto">
-        <span className="font-medium">Eagle AI Labs:</span>{" "}
-        {
-          "Partnership and Co-Ownership of a U.S. Regulated Crypto Hedgefund. This enables us to launch a new product, TradeDividends.  "
-        }{" "}
-        <Link href={"/learn-more"} className="font-medium underline">
-          Learn More
-        </Link>
-      </div>
+    <div className="container mx-auto rounded-xl mt-7 bg-banner py-3 px-4 text-center">
+      <span className="font-medium">Eagle AI Labs:</span>{" "}
+      {
+        "Partnership and Co-Ownership of a U.S. Regulated Crypto Hedgefund. This enables us to launch a new product, TradeDividends.  "
+      }{" "}
+      <Link href={"/learn-more"} className="font-medium underline">
+        Learn More
+      </Link>
     </div>
   );
 };
