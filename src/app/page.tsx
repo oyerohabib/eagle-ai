ea12b1438d2fe064d0f3cf2384bdfb11eb328923
oyerohabib <oyerohabib@yahoo.com>
2025-03-02 22:18:16 +0100

    feat: added footer, coming soon, registeration section components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import type { NextPage } from "next";
 import AlertBanner from "./components/Banner";
+import ComingSection from "./components/ComingSoon";
 import DividendsSection from "./components/Dividends";
 import FeatureSection from "./components/Feature";
+import Footer from "./components/Footer";
 import HeroSection from "./components/Hero";
 import Navbar from "./components/Navbar";
+import RegistrationSection from "./components/Registration";
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
     <div>
       <Navbar />
@@ -12,6 +16,11 @@ export default function Home() {
       <HeroSection />
       <FeatureSection />
       <DividendsSection />
+      <RegistrationSection />
+      <ComingSection />
+      <Footer />
     </div>
   );
-}
+};
+
+export default Home;
