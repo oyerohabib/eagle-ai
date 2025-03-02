7e61210b97fd38c179619dcf751a8415d0252567
oyerohabib <oyerohabib@yahoo.com>
2025-03-02 14:04:46 +0100

    style: improved hero section spacings, color, etc.

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -5,15 +5,15 @@ import MobileAppPreview from "@/app/assets/mobile-app-preview.png";
 
 const HeroSection: React.FC = () => {
   return (
-    <div className="bg-navy-900 text-white py-20 relative overflow-hidden">
+    <div className="text-white py-10 relative overflow-hidden">
       {/* Background wave effect - I willl be using SVG or CSS to achieve this */}
       <div className="container mx-auto px-6 flex flex-col lg:flex-row items-center">
         <div className="w-full lg:w-3/5 mb-12 lg:mb-0">
-          <h1 className="text-7xl text-blue-400 leading-[108px] font-poppins mb-6">
+          <h1 className="text-7xl leading-[108px] font-poppins mb-7 text-gradient">
             Pioneering the Future of Digital Asset Investment
           </h1>
 
-          <p className="text-lg leading-7 mb-8 font-poppins">
+          <p className="text-lg leading-7 mb-7 font-poppins">
             Eagle AI Labs is revolutionizing crypto asset management through our
             ground-breaking partnership with a U.S.-regulated crypto hedge fund.
             This exclusive collaboration combines our advanced AI-powered BTC
