e7aab32e4a84d112cde13e1f308e3ba3b6223b3e
oyerohabib <oyerohabib@yahoo.com>
2025-03-02 22:15:13 +0100

    feat: developed footer component

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/app/assets/logo.svg";
+import { FaTelegramPlane } from "react-icons/fa";
+import { FaMedium, FaXTwitter } from "react-icons/fa6";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-navy-900 text-white py-6">
+      <div className="container mx-auto px-6 flex flex-col md:flex-row justify-between items-center">
+        <div>
+          <Image src={Logo} alt="Eagle AI Logo" width={200} height={30} />
+        </div>
+
+        <p className="text-text font-openSans">
+          Copyright &copy; {new Date().getFullYear()} Eagle AI
+        </p>
+
+        <div className="flex items-center justify-between gap-8">
+          <Link href="/" className="text-footer-icons">
+            <FaTelegramPlane className="w-6 h-6" />
+          </Link>
+          <Link href="/" className="text-footer-icons">
+            <FaXTwitter className="w-6 h-6" />
+          </Link>
+          <Link href="/" className="text-footer-icons">
+            <FaMedium className="w-6 h-6" />
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
