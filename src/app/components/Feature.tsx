7d80f4fa8a33c9c1f30345e7702bdbd0e6682658
oyerohabib <oyerohabib@yahoo.com>
2025-03-02 22:05:51 +0100

    fix: improved styling and apperance of Feature component

--- src/app/components/Feature.tsx
+++ src/app/components/Feature.tsx
@@ -2,10 +2,13 @@ import Image from "next/image";
 import { FeatureListProps, FeatureListItemProps } from "../types";
 import Crypto from "@/app/assets/crypto-portal-image.png";
 
-const FeatureListItem: React.FC<FeatureListItemProps> = ({ text }) => {
+const FeatureListItem: React.FC<FeatureListItemProps> = ({
+  text,
+  className,
+}) => {
   return (
-    <li className="flex items-start mb-2">
-      <span className="text-blue-400 mr-2">•</span>
+    <li className={`$flex items-start mb-2 ${className}`}>
+      <span className={`text-blue-400 mr-2 ${className}`}>•</span>
       <span>{text}</span>
     </li>
   );
@@ -13,9 +16,13 @@ const FeatureListItem: React.FC<FeatureListItemProps> = ({ text }) => {
 
 export const FeatureList: React.FC<FeatureListProps> = ({ items }) => {
   return (
-    <ul className="text-gray-300 text-sm">
+    <ul className={`text-text text-base leading-[22px] font-openSans`}>
       {items.map((item, index) => (
-        <FeatureListItem key={index} text={item} />
+        <FeatureListItem
+          key={index}
+          text={item}
+          className="text-eagle-white text-lg"
+        />
       ))}
     </ul>
   );
@@ -33,22 +40,24 @@ const FeatureSection: React.FC = () => {
   return (
     <div className="text-white py-20">
       <div className="container mx-auto px-6 flex flex-col lg:flex-row items-center">
-        <div className="w-full lg:w-1/2 mb-12 lg:mb-0">
-          <Image
-            src={Crypto}
-            alt="Eagle AI Crypto Portal"
-            width={500}
-            height={400}
-            className="rounded-lg"
-          />
+        <div className="w-full lg:w-2/5 mb-12 lg:mb-0">
+          <div className="border">
+            <Image
+              src={Crypto}
+              alt="Eagle AI Crypto Portal"
+              width={450}
+              height={400}
+              className="rounded-lg"
+            />
+          </div>
         </div>
 
-        <div className="w-full lg:w-1/2 pl-0 lg:pl-12">
-          <h2 className="text-4xl font-bold mb-6">
+        <div className="w-full lg:w- pl-0 lg:pl-12">
+          <h2 className="text-5xl leading-[72px] mb-4 font-poppins">
             A new era in crypto investment
           </h2>
 
-          <p className="text-gray-300 mb-4">
+          <p className="mb-4 font-poppins">
             Our partnership represents a milestone in institutional crypto
             adoption, featuring:
           </p>
