b89c92311e630a04cceb48db23d64fa9dfb5ecdc
oyerohabib <oyerohabib@yahoo.com>
2025-03-03 00:02:47 +0100

    fix: improved it to handle controlled state

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -4,15 +4,26 @@ interface InputProps {
   type: string;
   name: string;
   placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Input: React.FC<InputProps> = ({ type, name, placeholder }) => {
+const Input: React.FC<InputProps> = ({
+  type,
+  name,
+  placeholder,
+  value,
+  onChange,
+}) => {
   return (
     <div>
       <input
         type={type}
         name={name}
         placeholder={placeholder}
+        required
+        value={value}
+        onChange={onChange}
         className="w-full p-5 rounded-3xl text-white placeholder-placeholder-text bg-[#344255] focus:outline-none transition duration-300 focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
       />
     </div>
