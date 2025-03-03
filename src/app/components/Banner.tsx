11375f0ea2c9abdf70beda11ee40fb8305c6ed36
oyerohabib <oyerohabib@yahoo.com>
2025-03-03 14:10:14 +0100

    feat: added responsivness to navbar

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -2,14 +2,16 @@ import Link from "next/link";
 
 const AlertBanner: React.FC = () => {
   return (
-    <div className="container mx-auto rounded-xl mt-7 bg-banner py-3 px-4 text-center">
-      <span className="font-medium">Eagle AI Labs:</span>{" "}
-      {
-        "Partnership and Co-Ownership of a U.S. Regulated Crypto Hedgefund. This enables us to launch a new product, TradeDividends.  "
-      }{" "}
-      <Link href={"/learn-more"} className="font-medium underline">
-        Learn More
-      </Link>
+    <div className="mx-8">
+      <div className="container mx-auto rounded-xl mt-7 bg-banner py-3 px-4 text-center">
+        <span className="font-medium">Eagle AI Labs:</span>{" "}
+        {
+          "Partnership and Co-Ownership of a U.S. Regulated Crypto Hedgefund. This enables us to launch a new product, TradeDividends.  "
+        }{" "}
+        <Link href={"/learn-more"} className="font-medium underline">
+          Learn More
+        </Link>
+      </div>
     </div>
   );
 };
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,8 +1,12 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import Button from "./Button";
 import { NavItemProps } from "../types";
 import Logo from "@/app/assets/logo.svg";
+import LogoIcon from "@/app/assets/logo-icon.svg";
+import HamburgerIcon from "@/app/assets/menu.svg";
+import { useState, useEffect } from "react";
 
 const NavItem: React.FC<NavItemProps> = ({ title, href, active }) => {
   return (
@@ -28,15 +32,76 @@ const Navbar: React.FC = () => {
     { title: "Trade Dividends", href: "/trade-dividends", active: true },
   ];
 
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [isLargeScreen, setIsLargeScreen] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth >= 1280);
+    };
+
+    // Initial check
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
   return (
     <div className="bg-navbar">
-      <nav className="container mx-auto px-6 py-4 flex items-center">
-        <div className="w-full flex items-center justify-between">
-          <Link href="/" className="mr-10">
-            <Image src={Logo} alt="Eagle AI Logo" width={120} height={32} />
-          </Link>
+      <nav className="container mx-auto px-6 py-4 flex items-center justify-evenly">
+        <Link href="/" className="mr-auto">
+          <Image
+            src={isLargeScreen ? Logo : LogoIcon}
+            alt="Eagle AI Logo"
+            width={isLargeScreen ? 120 : 40}
+            height={32}
+            className="hidden xl:block"
+          />
+          <Image
+            src={LogoIcon}
+            alt="Eagle AI Logo"
+            width={40}
+            height={32}
+            className="xl:hidden"
+          />
+        </Link>
 
-          <div className="hidden md:flex gap-10">
+        <div className="xl:hidden">
+          <Image
+            src={HamburgerIcon}
+            alt="Menu"
+            width={30}
+            height={30}
+            onClick={toggleMenu}
+            className="cursor-pointer"
+          />
+        </div>
+
+        <div className="hidden xl:flex items-center justify-between gap-14">
+          {navItems.map((item) => (
+            <NavItem
+              key={item.title}
+              title={item.title}
+              href={item.href}
+              active={item.active}
+            />
+          ))}
+          <Button text="Launch App" href="/" />
+        </div>
+
+        <div
+          className={`absolute top-16 left-0 w-full z-10 bg-navbar p-2 xl:hidden flex flex-col gap-4 transition-all duration-300 ease-in-out transform ${
+            menuOpen
+              ? "opacity-100 translate-y-0"
+              : "opacity-0 -translate-y-full pointer-events-none"
+          }`}
+        >
+          <div className="container mx-auto px-4 flex flex-col gap-4">
             {navItems.map((item) => (
               <NavItem
                 key={item.title}
@@ -45,8 +110,8 @@ const Navbar: React.FC = () => {
                 active={item.active}
               />
             ))}
+            <Button text="Launch App" href="/" className="w-full" />
           </div>
-          <Button text="Launch App" href="/" className="ml-4" />
         </div>
       </nav>
     </div>
