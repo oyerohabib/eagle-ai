9f23491e279e28aabb807fac40e8b6bb36bcdfee
oyerohabib <oyerohabib@yahoo.com>
2025-03-04 01:52:46 +0100

    fix: improved responsiveness of Footer component

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -6,8 +6,8 @@ import { FaMedium, FaXTwitter } from "react-icons/fa6";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-navy-900 text-white py-6">
-      <div className="container mx-auto px-6 flex flex-col md:flex-row justify-between items-center">
+    <footer className="text-white py-16 md:py-10 bg-transparent">
+      <div className="container mx-auto px-6 flex flex-col md:flex-row gap-6 justify-between items-center">
         <div>
           <Image src={Logo} alt="Eagle AI Logo" width={200} height={30} />
         </div>
