84cecbe41d9c27f33cc765dc473d300002ec6a25
oyerohabib <oyerohabib@yahoo.com>
2025-03-04 01:51:15 +0100

    fix: improved responsiveness of Dividends component

--- src/app/components/Dividends.tsx
+++ src/app/components/Dividends.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import DividendsIllustration from "@/app/assets/dividends-illustration.png";
+import Dividends from "@/app/assets/dividends.png";
+import Dividends2 from "@/app/assets/dividends-2.png";
 import { FeatureList } from "./Feature";
 
 const DividendsSection: React.FC = () => {
@@ -10,11 +11,11 @@ const DividendsSection: React.FC = () => {
   ];
 
   return (
-    <div className="bg-navy-900 text-white py-16">
+    <div className="text-white lg:py-20 py-10">
       <div className="container mx-auto px-6">
-        <div className="flex flex-col lg:flex-row items-start mb-16">
-          <div className="w-full lg:w-1/2 mb-8 lg:mb-0">
-            <h2 className="text-5xl leading-[72px] mb-4 font-poppins">
+        <div className="flex flex-col lg:flex-row items-start">
+          <div className="w-full lg:w-1/2 mb-0 lg:mb-8 order-2 lg:order-1">
+            <h2 className="lg:text-5xl text-4xl leading-12 lg:leading-[72px] mb-4 font-poppins">
               Trade Dividends Rewarding our comunity
             </h2>
 
@@ -26,15 +27,26 @@ const DividendsSection: React.FC = () => {
             <FeatureList items={benefits} />
           </div>
 
-          <div className="w-full lg:w-1/2 flex justify-center lg:justify-end">
+          <div className="w-full lg:w-1/2 flex mb-12 lg:mb-0 justify-center lg:justify-end order-1 lg:order-2">
             <div className="border">
-              <Image
-                src={DividendsIllustration}
-                alt="Trade Dividends Illustration"
-                width={400}
-                height={300}
-                className="rounded-lg"
-              />
+              <div className="hidden md:block">
+                <Image
+                  src={Dividends}
+                  alt="Trade Dividends Illustration"
+                  width={400}
+                  height={300}
+                  className="rounded-lg"
+                />
+              </div>
+              <div className="block md:hidden">
+                <Image
+                  src={Dividends2}
+                  alt="Trade Dividends Illustration"
+                  width={400}
+                  height={300}
+                  className="rounded-lg"
+                />
+              </div>
             </div>
           </div>
         </div>
