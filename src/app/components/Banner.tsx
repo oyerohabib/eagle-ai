d1aa7583c2d745600d0938178f6d8e1b3ea164e0
oyerohabib <oyerohabib@yahoo.com>
2025-03-04 01:50:40 +0100

    fix: improved responsiveness of Banner component

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -2,15 +2,17 @@ import Link from "next/link";
 
 const AlertBanner: React.FC = () => {
   return (
-    <div className="mx-8">
+    <div className="mx-8" id="fullText">
       <div className="container mx-auto rounded-xl mt-7 bg-banner py-3 px-4 text-center">
-        <span className="font-medium">Eagle AI Labs:</span>{" "}
-        {
-          "Partnership and Co-Ownership of a U.S. Regulated Crypto Hedgefund. This enables us to launch a new product, TradeDividends.  "
-        }{" "}
-        <Link href={"/learn-more"} className="font-medium underline">
-          Learn More
-        </Link>
+        <div className="whitespace-nowrap overflow-hidden text-ellipsis">
+          <span className="font-medium">Eagle AI Labs:</span>{" "}
+          {
+            "Partnership and Co-Ownership of a U.S. Regulated Crypto Hedgefund. This enables us to launch a new product, TradeDividends.  "
+          }{" "}
+          <Link href={"/learn-more"} className="font-medium underline">
+            Learn More
+          </Link>
+        </div>
       </div>
     </div>
   );
