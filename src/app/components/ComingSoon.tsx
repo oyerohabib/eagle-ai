5973911ffa5acf8d850ba78f407eb207fa7d6a82
oyerohabib <oyerohabib@yahoo.com>
2025-03-04 01:50:57 +0100

    fix: improved responsiveness of comingSoon component

--- src/app/components/ComingSoon.tsx
+++ src/app/components/ComingSoon.tsx
@@ -9,9 +9,9 @@ const ComingSection: React.FC = () => {
   ];
 
   return (
-    <div className="btn-gradient text-white py-12">
+    <div className="btn-gradient text-white py-12 mx-5 md:mx-0">
       <div className="container mx-auto px-6">
-        <h2 className="text-5xl leading-[72px] font-poppins mb-6">
+        <h2 className="md:text-5xl text-4xl md:leading-[72px] font-poppins mb-6">
           Coming Q1 2025
         </h2>
 
